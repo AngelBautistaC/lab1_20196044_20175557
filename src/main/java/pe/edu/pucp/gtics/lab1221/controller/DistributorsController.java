79f6de84c08188515db74557ad88cee014070ebe
Angel Alvaro Bautista Cutipa <a20196044@pucp.edu.pe>
2022-09-06 20:29:06 -0500

    avance distribuidoras

--- src/main/java/pe/edu/pucp/gtics/lab1221/controller/DistributorsController.java
+++ src/main/java/pe/edu/pucp/gtics/lab1221/controller/DistributorsController.java
@@ -1,10 +1,32 @@
 package pe.edu.pucp.gtics.lab1221.controller;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pe.edu.pucp.gtics.lab1221.entity.Distributors;
+import pe.edu.pucp.gtics.lab1221.repository.DistributorsRepository;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/distribuidoras")
 public class DistributorsController {
 
-    public String listaDistribuidoras (){
-        return "";
-    };
+    @Autowired
+    DistributorsRepository distributorsRepository;
+
+
+    @GetMapping(value={"/lista"})
+    public String listaDistribuidoras (Model model){
+        List<Distributors> lista=distributorsRepository.findAll(Sort.by("nombre"));
+        model.addAttribute("distribuidoraslista",lista);
+
+        return "distribuidoras/lista";
+    }
 
     public String editarDistribuidoras(){
         return "";
